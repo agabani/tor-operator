3bcde3934b269e7b811b588559c972523050f787
agabani <agabani@users.noreply.github.com>
2021-03-11 21:43:49 +0000

    feat(daemon): add tor hidden service
    Add Tor hidden service to daemon

---
+++ tor-operator-daemon/src/main.rs
@@ -0,0 +1,14 @@
+use libtor::{HiddenServiceVersion, Tor, TorAddress, TorFlag};
+
+fn main() {
+    Tor::new()
+        .flag(TorFlag::DataDirectory("/tmp/tor-rust".into()))
+        .flag(TorFlag::HiddenServiceDir("/tmp/tor-rust/hs-dir".into()))
+        .flag(TorFlag::HiddenServiceVersion(HiddenServiceVersion::V3))
+        .flag(TorFlag::HiddenServicePort(
+            TorAddress::Port(80),
+            Some(TorAddress::AddressPort("127.0.0.1".into(), 8080)).into(),
+        ))
+        .start()
+        .unwrap();
+}
